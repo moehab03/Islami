e4fd418f296fe7a752edd880167f75e4106e0ec2
Mohamed Ehab <120674384+moehab03@users.noreply.github.com>
2024-01-17 12:22:45 +0200

    Ahadith adapter is done.

---
+++ app/src/main/java/com/example/islami/adapter/AhadethAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.islami.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.example.islami.R
+
+class AhadethAdapter(private var ahadethList: ArrayList<String> = ArrayList()) : Adapter<AhadethAdapter.ViewHolder>() {
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var hadeth: TextView = itemView.findViewById(R.id.hadeth_tv)
+    }
+
+    override fun getItemCount(): Int = ahadethList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder = ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.ahadeth_view, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.hadeth.text = ahadethList[position]
+    }
+}
---
+++ app/src/main/java/com/example/islami/ui/screens/ahadeth/AhadethFragment.kt
@@ -0,0 +1,39 @@
+package com.example.islami.ui.screens.ahadeth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.islami.adapter.AhadethAdapter
+import com.example.islami.databinding.FragmentAhadethBinding
+
+class AhadethFragment : Fragment() {
+
+    private lateinit var binding: FragmentAhadethBinding
+    lateinit var adapter : AhadethAdapter
+    private var ahadethList = ArrayList<String>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAhadethBinding.inflate(inflater, container, false)
+        fillAhadethArray()
+        adapter = AhadethAdapter(ahadethList)
+        binding.ahadethRecyclerView.adapter = adapter
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun fillAhadethArray() {
+        for (x in 1..55) {
+            ahadethList.add("الحديث رقم $x")
+        }
+    }
+
+}
